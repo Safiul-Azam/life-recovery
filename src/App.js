a073876930c36f34972ed93adfebb7902b6ed925
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-26 19:31:22 +0600

    installed firebase and some change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./pages/Shared/Navbar";
 
 const App = () => {
   return (
     <main>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
