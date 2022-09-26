954ea9a08e0a539bcdd257793366e7927eb202b5
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-26 22:13:17 +0600

    update 26-9-22

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -53,7 +53,7 @@ export function Counter() {
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
-          Add Async
+          Add Async data
         </button>
         <button
           className={styles.button}
