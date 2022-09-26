6a650e3a2e843e5bf7967d43dbf1e2053336baa7
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-26 18:04:15 +0600

    routes setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Project</h1>
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
   );
 };
 
