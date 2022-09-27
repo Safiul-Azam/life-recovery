feaf6f59c2cd2e1e51653e0bf46bdcfb130e1dbf
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-27 10:43:34 +0600

    Require auth implement in for home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
 import Home from "./pages/Home";
 import Navbar from "./pages/Shared/Navbar";
+import RequireAuth from "./pages/Authentication/RequireAuth";
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<RequireAuth><Home/></RequireAuth>} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- src/pages/Authentication/RequireAuth.jsx
+++ src/pages/Authentication/RequireAuth.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
-import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
+import auth from '../../firebase.inite';
 
 const RequireAuth = ({children}) => {
     const [user, loading] = useAuthState(auth)
     // const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation()
     if(loading){
-        return <Loading></Loading>
+        return <p>Loading...</p>
     }
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-    // if(!user.emailVerified){
-    //     return <div className='text-center my-5 border p-5 w-50 mx-auto'>
-    //     <h2 className='title-color'>Your Email is not verified</h2>
-    //     <h4>Please verify your email</h4>
-    //     <button
-    //         onClick={async () => {
-    //             await sendEmailVerification();
-    //             toast('Sent new verification email');
-    //         }} className='rounded-0 my-3 btn btn-secondary glass' variant="dark"
-    //     >
-    //        New verify email</button>
-    // </div>
-    // }
     return children
 };
 
