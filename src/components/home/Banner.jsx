74b81ee634bddb9b8d905d5e5aef478f96a43574
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-01 06:44:32 +0600

    Calculations  selected Namaz

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = () => {
   return (
     <section className="pt-6 min-h-screen">
       <Navbar />
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center content-center max-w-7xl mx-auto gap-10 min-h-[50vh] pt-16 md:mt-0">
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center content-center max-w-7xl mx-auto gap-10 px-10 pt-16 md:mt-0">
         <DayManage />
         <Namaz />
         <ComposedCharts />
--- src/components/home/Calculations.jsx
+++ src/components/home/Calculations.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Calculation from "./Calculation";
 
 const Calculations = () => {
+  const [selectedNamaz, setSelectedNamaz] = useState("ফজর")
+
+  const namazName = ["ফজর", "যোহর", "আসর", "মাগরিব", "এশা"];
+
   return (
     <section className="max-w-7xl px-10 mx-auto">
-      <Calculation namaz="ফজর" />
-      <Calculation namaz="যোহর" />
-      <Calculation namaz="আসর" />
-      <Calculation namaz="মাগরিব" />
-      <Calculation namaz="এশা" />
-      
+      <div className="flex gap-1 justify-center items-center content-center mt-16 mb-5">
+        {namazName.map((namaz) => (
+          <h6
+            onClick={() => setSelectedNamaz(namaz)}
+            className={`px-4 py-2 btn ${
+              selectedNamaz === namaz
+                ? "btn-secondary text-black"
+                : "btn-primary text-white"
+            } `}
+          >
+            {namaz}
+          </h6>
+        ))}
+      </div>
+      <div className="flex flex-col gap-10 justify-center items-center content-center">
+        <Calculation namaz={selectedNamaz} />
+      </div>
     </section>
   );
 };
