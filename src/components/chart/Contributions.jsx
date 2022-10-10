4174451e456a7b980d97ddc4f525dd455fc975bd
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-10 21:07:07 +0600

    less and more color set

--- src/components/chart/Contributions.jsx
+++ src/components/chart/Contributions.jsx
@@ -15,7 +15,14 @@ const Contributions = () => {
     "2022-10-17": 5,
     "2022-10-18": 3,
   };
-  var panelColors = ["#EBEDF0", "rgba(196, 46, 0, 0.822)", "rgb(251, 255, 0)", "rgb(138, 115, 255)","rgba(1, 48, 177, 0.815)","rgb(25, 202, 2)"];
+  var panelColors = [
+    "#EBEDF0",
+    "rgba(196, 46, 0, 0.822)",
+    "rgb(251, 255, 0)",
+    "rgb(138, 115, 255)",
+    "rgba(1, 48, 177, 0.815)",
+    "rgb(25, 202, 2)",
+  ];
   var until = "2022-12-30";
   var weekNames = ["s", "m", "t", "w", "t", "f", "s"];
   var monthNames = [
@@ -36,23 +43,22 @@ const Contributions = () => {
     style: {
       "font-size": 14,
       "alignment-baseline": "central",
-      "margin":'20px',
+      margin: "20px",
       fill: "#AAA",
     },
   };
-  var panelAttributes = { rx: 2, ry: 2 };
+  var panelAttributes = { rx: 6, ry: 2 };
   var weekLabelAttributes = {
     style: {
       "font-size": 14,
       "alignment-baseline": "central",
-      "margin-bottom":'20px',
       fill: "#AAA",
     },
   };
 
   return (
-    <div className="my-20">
-      <div className="w-1/2 mx-auto ">
+    <div className="my-20 flex justify-center">
+      <div className="bg-[#162033] w-1/2 p-10 mx-auto rounded-2xl">
         <Calendar
           values={values}
           panelColors={panelColors}
@@ -63,6 +69,15 @@ const Contributions = () => {
           monthNames={monthNames}
           weekLabelAttributes={weekLabelAttributes}
         />
+        <div className="flex items-center space-x-1">
+          <p className="text-white text-sm">Less</p>
+          <p className="w-3 h-3 bg-green-300"></p>
+          <p className="w-3 h-3 bg-[#FBFF00]"></p>
+          <p className="w-3 h-3 bg-[#052D99]"></p>
+          <p className="w-3 h-3 bg-[#8A73FF]"></p>
+          <p className="w-3 h-3 bg-[#19CA02]"></p>
+          <p className="text-sm text-white">More</p>
+        </div>
       </div>
     </div>
   );
