3993c560aa2202458c4043ff317fa43761fb110c
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-10 23:19:52 +0600

    contribution design up to date

--- src/components/chart/Contributions.jsx
+++ src/components/chart/Contributions.jsx
@@ -57,27 +57,33 @@ const Contributions = () => {
   };
 
   return (
-    <div className="my-20 flex justify-center">
-      <div className="bg-[#162033] w-1/2 p-10 mx-auto rounded-2xl">
-        <Calendar
-          values={values}
-          panelColors={panelColors}
-          panelAttributes={panelAttributes}
-          until={until}
-          weekNames={weekNames}
-          monthLabelAttributes={monthLabelAttributes}
-          monthNames={monthNames}
-          weekLabelAttributes={weekLabelAttributes}
-        />
-        <div className="flex items-center space-x-1">
-          <p className="text-white text-sm">Less</p>
-          <p className="w-3 h-3 bg-green-300"></p>
-          <p className="w-3 h-3 bg-[#FBFF00]"></p>
-          <p className="w-3 h-3 bg-[#052D99]"></p>
-          <p className="w-3 h-3 bg-[#8A73FF]"></p>
-          <p className="w-3 h-3 bg-[#19CA02]"></p>
-          <p className="text-sm text-white">More</p>
+    <div className="my-20 max-w-5xl mx-auto bg-[#162033] ">
+      <h2 className="text-xl mb-3 text-white text-center">
+        Contributions Graph
+      </h2>
+      <div className="flex justify-center items-center">
+        <div className=" w-full mx-auto flex justify-center rounded-2xl">
+          <Calendar
+            values={values}
+            panelColors={panelColors}
+            panelAttributes={panelAttributes}
+            until={until}
+            weekNames={weekNames}
+            monthLabelAttributes={monthLabelAttributes}
+            monthNames={monthNames}
+            weekLabelAttributes={weekLabelAttributes}
+            className=""
+          />
         </div>
+      </div>
+      <div className="flex items-center space-x-1 mt-2 justify-end">
+        <p className="text-white text-sm">Less</p>
+        <p className="w-3 h-3 bg-green-300"></p>
+        <p className="w-3 h-3 bg-[#FBFF00]"></p>
+        <p className="w-3 h-3 bg-[#052D99]"></p>
+        <p className="w-3 h-3 bg-[#8A73FF]"></p>
+        <p className="w-3 h-3 bg-[#19CA02]"></p>
+        <p className="text-sm text-white">More</p>
       </div>
     </div>
   );
