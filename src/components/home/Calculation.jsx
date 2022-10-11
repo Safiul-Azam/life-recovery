02a7857017b6cf82cc41a025b1e3e24710c6d268
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-11 13:55:57 +0600

    Contributions Graph done & style update

--- src/components/home/Calculation.jsx
+++ src/components/home/Calculation.jsx
@@ -16,21 +16,21 @@ const Calculation = ({ namaz }) => {
   const { namaz: name, jamaat, takbir_e_ula, count, days } = data || {};
 
   return (
-    <section className="max-w-7xl mx-10 2xl:mx-auto bg-slate-800/[0.5] backdrop-blur-sm rounded-xl px-10 py-5">
+    <section className="max-w-7xl mx-auto ">
       <h1 className="text-primary text-2xl font-semibold font-mono w-full pb-2 text-center mb-3">
         {namaz}
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  justify-items-center items-center gap-3">
         {/* নামাজ */}
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-64 rounded-xl px-2 py-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-72 rounded-xl px-2 py-2">
           <h2 className="text-center text-slate-100 font-bold m-3 md:m-0">নামাজ</h2>
 
           <TwoSimplePieChart name={name} count={count} days={days} />
         </div>
 
         {/* কাজা নামাজ */}
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-64 rounded-xl px-2 py-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-72 rounded-xl px-2 py-2">
           <h2 className="text-center text-slate-100 font-bold px-2 m-3 md:m-0">
             কাজা নামাজ
           </h2>
@@ -39,7 +39,7 @@ const Calculation = ({ namaz }) => {
         </div>
 
         {/* জামাতে পড়েছেন  */}
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-64 rounded-xl px-2 py-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-72 rounded-xl px-2 py-2">
           <h2 className="text-center text-slate-100 font-bold m-3 md:m-0">
             জামাতে পড়েছেন
           </h2>
@@ -48,7 +48,7 @@ const Calculation = ({ namaz }) => {
         </div>
 
         {/* তাকবীরে উলার সাথে পড়েছেন */}
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-64 rounded-xl px-2 py-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-slate-900 w-full lg:w-72 rounded-xl px-2 py-2">
           <h2 className="text-center text-slate-100 font-bold m-3 md:m-0">
             তাকবীরে-উলার সাথে পড়েছেন
           </h2>
