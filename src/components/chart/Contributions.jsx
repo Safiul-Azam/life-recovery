a39c6402f4f01383566987d08ef6c726a734e241
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-11 08:46:56 +0600

    Contributions Graph style done

--- src/components/chart/Contributions.jsx
+++ src/components/chart/Contributions.jsx
@@ -27,8 +27,8 @@ const Contributions = () => {
     "#003cff",
     "#1eff00",
   ];
-  const until = "2022-12-30";
-  const weekNames = ["s", "m", "t", "w", "t", "f", "s"];
+  const until = "2022-10-15";
+  const weekNames = ["", "M", "", "W", "", "F", ""];
   const monthNames = [
     "Jan",
     "Feb",
@@ -45,51 +45,58 @@ const Contributions = () => {
   ];
   const monthLabelAttributes = {
     style: {
-      fontSize: 14,
+      fontSize: 12,
       alignmentBaseline: "central",
-      margin: "20px",
       fill: "#ffffff",
+
     },
   };
-  const panelAttributes = { rx: 6, ry: 2 };
+  const panelAttributes = { rx: 6, ry: 6 };
   const weekLabelAttributes = {
+    rotate: 20,
+
     style: {
-      fontSize: 14,
+      fontSize: 12,
       alignmentBaseline: "central",
       fill: "#ffffff",
     },
   };
 
   return (
-    <div className="my-20 max-w-5xl mx-auto bg-[#162033] ">
-      <h2 className="text-xl mb-3 text-white text-center">
+    <div className="my-20 max-w-7xl sm:px-10 mx-auto  px-5">
+      <h2 className="text-xl py-3 text-white text-center bg-[#162033] rounded-xl rounded-b-none">
         Contributions Graph
       </h2>
-      <div className="flex justify-center items-center">
-        <div className=" w-full mx-auto flex justify-center rounded-2xl">
+      <div className="flex justify-center items-center gap-3 px-5 pb-2 bg-[#162033] rounded-xl rounded-t-none">
+        <div className="flex flex-col justify-start w-full">
           <Calendar
             values={values}
             panelColors={panelColors}
             panelAttributes={panelAttributes}
             until={until}
-            weekNames={weekNames}
-            monthLabelAttributes={monthLabelAttributes}
             monthNames={monthNames}
+            monthLabelAttributes={monthLabelAttributes}
+            weekNames={weekNames}
             weekLabelAttributes={weekLabelAttributes}
-            className=""
           />
+          <div className="flex items-center space-x-1 mt-2 justify-start text-slate-200 text-sm px-4">
+            <p className="w-3 h-3 rounded-full bg-[#ff0000]"></p>
+            <span> = 0</span>
+            <p className="w-3 h-3 rounded-full bg-[#fc754cd2]"></p>{" "}
+            <span> = 1</span>
+            <p className="w-3 h-3 rounded-full bg-[#FBFF00]"></p>{" "}
+            <span> = 2</span>
+            <p className="w-3 h-3 rounded-full bg-[#00eeff]"></p>{" "}
+            <span> = 3</span>
+            <p className="w-3 h-3 rounded-full bg-[#003cff]"></p>{" "}
+            <span> = 4</span>
+            <p className="w-3 h-3 rounded-full bg-[#1eff00]"></p>{" "}
+            <span> = 5</span>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center space-x-1 mt-2 justify-end">
-        <p className="text-white text-sm">Less</p>
-        <p className="w-3 h-3 bg-[#ff0000]"></p>
-        <p className="w-3 h-3 bg-[#fc754cd2]"></p>
-        <p className="w-3 h-3 bg-[#FBFF00]"></p>
-        <p className="w-3 h-3 bg-[#00eeff]"></p>
-        <p className="w-3 h-3 bg-[#003cff]"></p>
-        <p className="w-3 h-3 bg-[#1eff00]"></p>
-        <p className="w-3 h-3 bg-[#ffffff]"></p>
-        <p className="text-sm text-white">More</p>
+        <div className="bg-white w-4/12 h-full">
+          <h1>hello</h1>
+        </div>
       </div>
     </div>
   );
