1657103cdd6536af270ce4381433572ada9c14f7
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-11 07:20:28 +0600

    react-github-contribution-calendar setup done

--- src/components/chart/Contributions.jsx
+++ src/components/chart/Contributions.jsx
@@ -2,30 +2,34 @@ import React from "react";
 import Calendar from "react-github-contribution-calendar";
 
 const Contributions = () => {
-  var values = {
-    "2022-04-23": 1,
-    "2022-06-26": 2,
-    "2022-09-29": 3,
-    "2022-10-9": 4,
-    "2022-11-08": 4,
-    "2022-10-12": 2,
-    "2022-10-13": 5,
-    "2022-10-14": 4,
-    "2022-10-15": 3,
-    "2022-10-17": 5,
-    "2022-10-18": 3,
+  const values = {
+    "2022-10-01": 0 + 1,
+    "2022-10-02": 0 + 1,
+    "2022-10-03": 1 + 1,
+    "2022-10-04": 1 + 1,
+    "2022-10-05": 2 + 1,
+    "2022-10-06": 2 + 1,
+    "2022-10-07": 3 + 1,
+    "2022-10-08": 3 + 1,
+    "2022-10-09": 4 + 1,
+    "2022-10-10": 4 + 1,
+    "2022-10-11": 5 + 1,
+    "2022-10-12": 5 + 1,
+    "2022-10-13": 4 + 1,
+    "2022-10-14": 5 + 1,
   };
-  var panelColors = [
-    "#EBEDF0",
-    "rgba(196, 46, 0, 0.822)",
-    "rgb(251, 255, 0)",
-    "rgb(138, 115, 255)",
-    "rgba(1, 48, 177, 0.815)",
-    "rgb(25, 202, 2)",
+  const panelColors = [
+    "#ffffff2f",
+    "#ff0000",
+    "#fc754cd2",
+    "#FBFF00",
+    "#00eeff",
+    "#003cff",
+    "#1eff00",
   ];
-  var until = "2022-12-30";
-  var weekNames = ["s", "m", "t", "w", "t", "f", "s"];
-  var monthNames = [
+  const until = "2022-12-30";
+  const weekNames = ["s", "m", "t", "w", "t", "f", "s"];
+  const monthNames = [
     "Jan",
     "Feb",
     "Mar",
@@ -39,20 +43,20 @@ const Contributions = () => {
     "Nov",
     "Dec",
   ];
-  var monthLabelAttributes = {
+  const monthLabelAttributes = {
     style: {
-      "font-size": 14,
-      "alignment-baseline": "central",
+      fontSize: 14,
+      alignmentBaseline: "central",
       margin: "20px",
-      fill: "#AAA",
+      fill: "#ffffff",
     },
   };
-  var panelAttributes = { rx: 6, ry: 2 };
-  var weekLabelAttributes = {
+  const panelAttributes = { rx: 6, ry: 2 };
+  const weekLabelAttributes = {
     style: {
-      "font-size": 14,
-      "alignment-baseline": "central",
-      fill: "#AAA",
+      fontSize: 14,
+      alignmentBaseline: "central",
+      fill: "#ffffff",
     },
   };
 
@@ -78,11 +82,13 @@ const Contributions = () => {
       </div>
       <div className="flex items-center space-x-1 mt-2 justify-end">
         <p className="text-white text-sm">Less</p>
-        <p className="w-3 h-3 bg-green-300"></p>
+        <p className="w-3 h-3 bg-[#ff0000]"></p>
+        <p className="w-3 h-3 bg-[#fc754cd2]"></p>
         <p className="w-3 h-3 bg-[#FBFF00]"></p>
-        <p className="w-3 h-3 bg-[#052D99]"></p>
-        <p className="w-3 h-3 bg-[#8A73FF]"></p>
-        <p className="w-3 h-3 bg-[#19CA02]"></p>
+        <p className="w-3 h-3 bg-[#00eeff]"></p>
+        <p className="w-3 h-3 bg-[#003cff]"></p>
+        <p className="w-3 h-3 bg-[#1eff00]"></p>
+        <p className="w-3 h-3 bg-[#ffffff]"></p>
         <p className="text-sm text-white">More</p>
       </div>
     </div>
